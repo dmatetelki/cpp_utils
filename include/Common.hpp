4082b369d7ae6160df2e23233713587a0c5d9a87
Denes Matetelki <denes.matetelki@gmail.com>
2011-02-27 01:03:19 +0100

    #else branch was missing

--- include/Common.hpp
+++ include/Common.hpp
@@ -3,4 +3,6 @@
 #ifndef NOTRACE
   #define TRACE(x) std::cout << x << " " << __PRETTY_FUNCTION__ <<  \
                           " @ " << __FILE__ << " : " << __LINE__ << std::endl
+#else
+  #define TRACE(x) ""
 #endif
