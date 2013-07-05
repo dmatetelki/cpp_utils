6bc4ddc4880b3e0a7712830a408c8228d1ff64a7
Denes Matetelki <denes.matetelki@gmail.com>
2013-07-05 20:02:01 +0200

    refactoring test/test_Message.hpp

--- test/test_Message.hpp
+++ test/test_Message.hpp
@@ -5,65 +5,68 @@
 #include "Message.hpp"
 #include "Connection.hpp"
 
+
+class SimpleMessage : public Message
+{
+public:
+
+  SimpleMessage( void *msgParam = 0)
+    : Message(msgParam)
+  {
+    TRACE;
+  }
+
+  bool buildMessage( const void   *msgPart,
+                     const size_t  msgLen )
+  {
+    TRACE;
+    m_buffer = std::string( (const char*) msgPart, msgLen );
+    onMessageReady();
+    return true;
+  }
+
+  void onMessageReady()
+  {
+    TRACE;
+
+    LOG_BEGIN(Logger::INFO)
+      LOG_PROP("message", m_buffer)
+    LOG_END("Got reply from server.");
+
+    *( static_cast<bool*>(m_param) ) = true;
+
+    if (m_connection != 0)
+      m_connection->send(m_buffer.c_str(), m_buffer.length());
+
+    m_buffer.clear();
+  }
+
+  Message* clone()
+  {
+    TRACE;
+    return new SimpleMessage(m_param);
+  }
+
+  std::string getBuffer()
+  {
+    TRACE;
+    return m_buffer;
+  }
+
+protected:
+
+  size_t getExpectedLength()
+  {
+    TRACE;
+    return 0;
+  }
+
+}; // SimpleMessage
+
+
 class TestMessage : public CxxTest::TestSuite
 {
 private:
-
-  class SimpleMessage : public Message
-  {
-  public:
-
-    SimpleMessage( void *msgParam = 0)
-      : Message(msgParam)
-    {
-      TRACE;
-    }
-
-    bool buildMessage( const void   *msgPart,
-                       const size_t  msgLen )
-    {
-      TRACE;
-      m_buffer = std::string( (const char*) msgPart, msgLen );
-      onMessageReady();
-      return true;
-    }
-
-    void onMessageReady()
-    {
-      TRACE;
-
-      LOG_BEGIN(Logger::INFO)
-        LOG_PROP("message", m_buffer)
-      LOG_END("Got reply from server.");
-
-      *( static_cast<bool*>(m_param) ) = true;
-
-      if (m_connection != 0)
-        m_connection->send(m_buffer.c_str(), m_buffer.length());
-    }
-
-    Message* clone()
-    {
-      TRACE;
-      return new SimpleMessage(m_param);
-    }
-
-    std::string getBuffer()
-    {
-      TRACE;
-      return m_buffer;
-    }
-
-  protected:
-
-    size_t getExpectedLength()
-    {
-      TRACE;
-      return 0;
-    }
-
-  }; // SimpleMessage
-
 
   class DummyConnection : public Connection
   {
